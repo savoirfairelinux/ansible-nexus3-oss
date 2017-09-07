5904212fa22f99f58e9066572d5348f9feb0bffb
Bernhard Strähle <bernhard.straehle@gmail.com>
2017-09-07 22:37:07 +0200

    typo

--- files/groovy/setup_http_proxy.groovy
+++ files/groovy/setup_http_proxy.groovy
@@ -15,7 +15,7 @@ if (parsed_args.with_http_proxy) {
 core.removeHTTPSProxy()
 if (parsed_args.with_https_proxy) {
     if (parsed_args.https_proxy_username) {
-        core.httpsProxyWithBasicAuth(parsed_args.https_proxy_host, parsed_args.https_proxy_port as int, parsed_args.http_proxy_username, parsed_args.https_proxy_password)
+        core.httpsProxyWithBasicAuth(parsed_args.https_proxy_host, parsed_args.https_proxy_port as int, parsed_args.https_proxy_username, parsed_args.https_proxy_password)
     } else {
         core.httpsProxy(parsed_args.https_proxy_host, parsed_args.https_proxy_port as int)
     }
