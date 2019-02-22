49aa7a867d8a10fe9add462b453d97632ef38071
Steven Edgar <steven.edgar@sky.uk>
2019-02-22 14:20:30 +0000

    Adding GCP Blobstore support

---
+++ files/groovy/create_blobstore_gcp.groovy
@@ -0,0 +1,21 @@
+import org.sonatype.nexus.blobstore.api.BlobStoreConfiguration
+import org.sonatype.nexus.blobstore.api.BlobStoreManager
+
+existingBlobstore = BlobStoreManager.get(parsed_args.name)
+
+if ( existingBlobstore != null) {
+    newConfig = existingBlobstore.configuration.copy()
+    newConfig.attributes['google cloud storage']['credentials'] = parsed_args.credentials
+
+    blobStoreManager.update(newConfig)
+} else {
+    def blobStoreManager = container.lookup(BlobStoreManager.class.name)
+    blobStoreManager.create(new BlobStoreConfiguration(name: parsed_args.name, type: 'Google Cloud Storage',
+    attributes: [
+        'google cloud storage': [
+            bucket: parsed_args.bucket,
+            credentials: parsed_args.credentials
+        ]
+    ]))
+}
+
