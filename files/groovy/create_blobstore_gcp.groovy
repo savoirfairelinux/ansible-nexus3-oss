6483f8252fc417e85f7e0a41b376e94ad9974ffd
Steven Edgar <steven.edgar@sky.uk>
2020-01-10 09:54:06 +0000

    Create blobstore groovy script fixes for 3.20

--- files/groovy/create_blobstore_gcp.groovy
+++ files/groovy/create_blobstore_gcp.groovy
@@ -1,5 +1,4 @@
 import groovy.json.JsonSlurper
-#import org.sonatype.nexus.blobstore.api.BlobStoreConfiguration
 import org.sonatype.nexus.blobstore.api.BlobStoreManager
 
 parsed_args = new JsonSlurper().parseText(args)
@@ -7,17 +6,6 @@ parsed_args = new JsonSlurper().parseText(args)
 existingBlobStore = blobStore.getBlobStoreManager().get(parsed_args.name)
 if (existingBlobStore == null) {
 
-#    def blobStoreManager = container.lookup(BlobStoreManager.class.name)
-#    blobStoreManager.create(new BlobStoreConfiguration(name: parsed_args.name, type: 'Google Cloud Storage',
-#    attributes: [
-#        'google cloud storage': [
-#            bucket: parsed_args.bucket,
-#            credential_file: parsed_args.credentials
-#            region: parsed_args.region
-#            name: parsed_args.name
-#        ]
-#    ]))
-    
     def config = blobStoreManager.newConfiguration()
     config.name = 'default'
     config.type = 'Google Cloud Storage'
